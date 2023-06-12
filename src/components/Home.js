49f1f0627f2fc45b58e4b554f75982ae13bbc73d
GabrielOchieng <ogingagabriel@gmail.com>
2023-06-12 11:22:02 +0300

    initial commit

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { HiArrowRight } from 'react-icons/hi';
+import {Link} from 'react-scroll';
+
+const Home = () => {
+  return (
+    <div name='home' className='w-full h-screen bg-[#0a192f]'>
+      {/* container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className='text-pink-600'>Hi, my name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Gabriel Ochieng Oginga</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Frontend Web Developer.</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>I am a Frontend Web Developer specializing in building exceptional digital experiences. Currently, I am focused on building responsive frontend web applications/websites.</p>
+        <div>
+        <Link to="work" smooth={true} duration={500}>
+        <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work 
+          <span className='group-hover:rotate-90 duration-300'><HiArrowRight className='ml-3'/></span> </button>
+        </Link> 
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Skills.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Css from '../assets/css.png';
+import Github from '../assets/github.png';
+import Html from '../assets/html.png';
+import Javascript from '../assets/javascript.png'
+import ReactJs from '../assets/ReactJS.png';
+
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+        {/* container */}
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div>
+            <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+            <p className='py-4'>These are the technologies I've worked with</p>
+        </div>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Html} alt="Html icon" />
+                <p className='my-4'>HTML</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Css} alt="Html icon" />
+                <p className='my-4'>CSS</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Javascript} alt="Html icon" />
+                <p className='my-4'>Javascript</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={ReactJs} alt="Html icon" />
+                <p className='my-4'>ReactJS</p>
+            </div>
+            <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                <img className='w-20 mx-auto' src={Github} alt="Html icon" />
+                <p className='my-4'>Github</p>
+            </div>
+        </div>
+        </div>
+        
+      
+    </div>
+  )
+}
+
+export default Skills
